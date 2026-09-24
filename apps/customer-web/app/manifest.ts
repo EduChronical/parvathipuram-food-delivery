9d417281ed0908852fb3cf74ed33ee4f2310a0d0
ieo-archives <harish1282001@gmail.com>
2026-09-24 16:15:45 +0530

    v3: add PWA manifest

---
+++ apps/customer-web/app/manifest.ts
@@ -0,0 +1,1 @@
+import type {MetadataRoute} from "next"; export default function manifest():MetadataRoute.Manifest{return {name:"PPM Bites",short_name:"PPM Bites",description:"Food delivery in Parvathipuram",start_url:"/",display:"standalone",background_color:"#f8f5ef",theme_color:"#e6502c",icons:[]}}
